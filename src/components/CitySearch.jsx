57bdea386088522b0737616a90a216b05df73d4b
Piyush Kukade <piyushkukade1@gmail.com>
2023-02-28 05:32:01 +0530

    Air Quality Index app

--- src/components/CitySearch.jsx
+++ src/components/CitySearch.jsx
@@ -1,100 +1,58 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import { getAirQuality } from '../api';
+import { getColor, getMaxAqiImpact } from '../utils';
 import '../styles/CitySearch.css';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import '../styles/CitySearch.css';
-import '../styles/AirQualityCard.css';
-
-function getColor(aqi) {
-  if (aqi <= 50) {
-    return 'green';
-  } else if (aqi <= 100) {
-    return 'yellow';
-  } else if (aqi <= 150) {
-    return 'orange';
-  } else if (aqi <= 200) {
-    return 'red';
-  } else if (aqi <= 300) {
-    return 'violet';
-  } else {
-    return 'darkred';
-  }
-}
-
-
-function getMaxAqiImpact(aqi) {
-  if (aqi <= 50) {
-    return <div style={{color: 'green'}}>Good</div>;
-  } else if (aqi <= 100) {
-    return <div style={{color: 'yellow'}}>Moderate</div>;
-  } else if (aqi <= 150) {
-    return <div style={{color: 'orange'}}>Unhealthy for Sensitive Groups</div>;
-  } else if (aqi <= 200) {
-    return <div style={{color: 'red'}}>Unhealthy</div>;
-  } else if (aqi <= 300) {
-    return <div style={{color: 'violet'}}>Very Unhealthy</div>;
-  } else {
-    return <div style={{color: 'darkred'}}>Hazardous</div>;
-  }
-}
 
 class CitySearch extends Component {
-  constructor(props) {
-    super(props);
+  //  state with cityName, airQualityData, isLoading, and error properties
+  state = {
+    cityName: '',
+    airQualityData: null,
+    isLoading: false,
+    error: null,
+  };
 
-    this.state = {
-      cityName: '',
-      airQualityData: null,
-      isLoading: false,
-      error: null
-    };
-
-    this.handleCityNameChange = this.handleCityNameChange.bind(this);
-    this.handleSearch = this.handleSearch.bind(this);
-  }
-
-  handleCityNameChange(event) {
+  // Handle changes
+  handleCityNameChange = (event) => {
     this.setState({ cityName: event.target.value });
-  }
+  };
 
-  
-
-  handleSearch() {
+  // Handle the search 
+  handleSearch = async () => {
     const { cityName } = this.state;
-    const url = `https://api.waqi.info/feed/${cityName}/?token=6162fe42bbef7e13590930efa4ac710203c66590`;
 
-    this.setState({ isLoading: true, error: null, airQualityData: null });
 
-    axios
-      .get(url)
-      .then(response => {
-        this.setState({ isLoading: false, airQualityData: response.data.data });
-      })
-      .catch(error => {
-        this.setState({ isLoading: false, error: error.message });
-      });
-  }
+    this.setState({ isLoading: true });
+
+    try {
+      //  function from the API module and store the response in airQualityData
+      const airQualityData = await getAirQuality(cityName);
+
+      //  airQualityData and isLoading to false
+      this.setState({ airQualityData, isLoading: false });
+    } catch (error) {
+      // set error and isLoading to false
+      this.setState({ error: error.message, isLoading: false });
+    }
+  };
 
   render() {
     const { cityName, airQualityData, isLoading, error } = this.state;
-  
-    // Get the current date and day
+
+    // current date and day and time
     const currentDate = new Date();
     const currentDay = currentDate.toLocaleString('default', { weekday: 'long' });
-  
+
+    //loading message
     if (isLoading) {
       return <div>Loading...</div>;
     }
-  
+
+    //  error message
     if (error) {
       return <div className="error-message">{error}</div>;
     }
-  
+
     return (
       <div className="city-search-container">
         <h2>Search Air Quality by City Name</h2>
@@ -126,7 +84,7 @@ class CitySearch extends Component {
                 <div className="aqi-impact">{getMaxAqiImpact(airQualityData.aqi)}</div>
               </div>
               <div>
-               
+              
               </div>
             </div>
           </div>
@@ -134,10 +92,6 @@ class CitySearch extends Component {
       </div>
     );
   }
-  
-  
-  
-
 }
-export default CitySearch;
 
+export default CitySearch;
